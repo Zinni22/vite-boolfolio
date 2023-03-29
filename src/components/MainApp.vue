5dc5885cbe01cf1b9d837ab4312cc180f8ac0120
Zineb Atlassi <atlassizineb@gmail.com>
2023-03-29 16:59:03 +0200

    Milestone 4

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,11 +1,23 @@
 <script>
+import axios from 'axios';
+
 export default {
 name: 'MainApp',
 
   data () {
       return{
-
+        projects: []
       }
+  },
+  created(){
+    axios
+    .get('http://127.0.0.1:8000/api/project')
+    .then(response => {
+        console.log(response.data.projects);
+
+        this.projects = response.data.projects;
+
+    })
   },
 
   components:{},
@@ -17,7 +29,7 @@ name: 'MainApp',
 <template>
 
     <main>
-        MAIN
+        <h1>Boolfolio</h1>
     </main>
  
 </template>
